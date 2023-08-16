235dc3b3087e052ceb1571bbc58e490ffce2b3c5
manash166 <manash166@gmail.com>
2023-08-16 15:36:29 +0530

    fist comit

---
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+TextView tex;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    tex=findViewById(R.id.textview);
+    tex.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+        }
+    });
+    }
+
+}
