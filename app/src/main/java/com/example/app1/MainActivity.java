ecccbe8b83a27cd6f0075a63d61ddc91ada42cc8
manash166 <manash166@gmail.com>
2023-08-19 14:40:09 +0530

    oo

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -6,8 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 Button btn;
@@ -19,8 +19,9 @@ Button btn;
     btn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Intent intent =new Intent(MainActivity.this, registration.class);
-            startActivity(intent);
+          Intent intent= new Intent(MainActivity.this,registration.class);
+          startActivity(intent);
+
         }
     });
     }
