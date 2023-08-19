5eb34e3a57719c5887538bfc8f803ae11ad3fae1
manash166 <manash166@gmail.com>
2023-08-19 16:43:45 +0530

    first
    registration12

--- app/src/main/java/com/example/app1/registration.java
+++ app/src/main/java/com/example/app1/registration.java
@@ -2,24 +2,13 @@ package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class registration extends AppCompatActivity {
-Button register_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-//        register_btn = findViewById(R.id.register_btn);
-//        register_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent=new Intent(registration.this, otp_screen.class);
-//            }
-//        });
-
     }
 }
