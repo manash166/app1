aa9760aa3652c3ca6dfe94b6da3309cc95f57129
manash166 <manash166@gmail.com>
2023-08-20 14:11:29 +0530

    20Aug_pc_1

--- app/src/main/java/com/example/app1/otp_screen.java
+++ app/src/main/java/com/example/app1/otp_screen.java
@@ -2,13 +2,92 @@ package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class otp_screen extends AppCompatActivity {
+
+    EditText editTextNumber, editTextNumber2, editTextNumber3, editTextNumber4;
+    private Button verify_btn;
+    TextView display_mobile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_screen);
+        display_mobile = findViewById(R.id.textView3);
+        editTextNumber = findViewById(R.id.editTextNumber);
+        editTextNumber2 = findViewById(R.id.editTextNumber2);
+        editTextNumber3 = findViewById(R.id.editTextNumber3);
+        editTextNumber4 = findViewById(R.id.editTextNumber4);
+        Intent mobile_display_intent = getIntent();
+        String mobile = mobile_display_intent.getStringExtra("mobile");
+        display_mobile.setText(String.format("+91" + mobile));
+        numbermove();
+    }
+
+    private void numbermove() {
+        editTextNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int count, int after) {
+                if (!s.toString().trim().isEmpty()) {
+                    editTextNumber2.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        editTextNumber2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int count, int after) {
+                if (!s.toString().trim().isEmpty()) {
+                    editTextNumber3.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        editTextNumber3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int count, int after) {
+                if (!s.toString().trim().isEmpty()) {
+                    editTextNumber4.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/app1/registration.java
+++ app/src/main/java/com/example/app1/registration.java
@@ -6,18 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class registration extends AppCompatActivity {
 Button register_btn;
+  EditText mobilenumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_registration);
-          register_btn=findViewById(R.id.register_btn);
+         mobilenumber=findViewById(R.id.editTextPhone2);
+         register_btn=findViewById(R.id.register_btn);
           register_btn.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View view) {
                   Intent intent= new Intent(registration.this, otp_screen.class);
+                  intent.putExtra("mobile",mobilenumber.getText().toString().trim());
                   startActivity(intent);
 
               }
